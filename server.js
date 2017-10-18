0decd2e837cb2a2d28d841c2c4e3cf4c581bf4eb
tomasz kolonko <tz.kolonko@gmail.com>
2017-10-18 21:55:38 +0200

    using express routes for REST APIs

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
 var express = require('express');
 var morgan = require('morgan');
-var http = require('http');
+var bodyParser = require('body-parser');
 
 var hostname = 'localhost';
 var port = 3000;
 
 var app = express();
+
 app.use(morgan('dev'));
 
-app.use(function (req, res, next) {
-  console.log(req.headers);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<html><body><h1>Hello World</h1></body></html>');
+var activityRouter = express.Router();
 
+activityRouter.use(bodyParser.json());
+
+activityRouter.route('/')
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+}).get(function(req,res,next){
+        res.end('Providing all activities!');
+}).post(function(req, res, next) {
+    res.end('Post activity: ' + req.body.name + ' with description: ' + req.body.description);
+}).delete(function(req, res, next) {
+        res.end('Deleting all activities');
 });
 
-var server = http.createServer(app);
+activityRouter.route('/:activityID')
+.all(function(req,res,next) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      next();
+}).get(function(req,res,next) {
+        res.end('Will send details of the activity: ' + req.params.activityID
+          +' to you!');
+}).put(function(req, res, next) {
+        res.write('Updating the activity: ' + req.params.activityID + '\n');
+    res.end('Will update the activity: ' + req.body.name +
+            ' with details: ' + req.body.description);
+}).delete(function(req, res, next) {
+        res.end('Deleting activity: ' + req.params.activityID);
+});
 
-server.listen(port, hostname, function(){
+app.use('/activity', activityRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function(){
   console.log(`Server running at http://${hostname}:${port}/`);
 });

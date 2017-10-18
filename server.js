1e1a9d8d26c03a5a15f14e30569ad8fb846b5d9d
tomasz kolonko <tz.kolonko@gmail.com>
2017-10-18 19:42:44 +0200

    added morgan package

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express'),
-     http = require('http');
+var express = require('express');
+var morgan = require('morgan');
+var http = require('http');
 
 var hostname = 'localhost';
 var port = 3000;
 
 var app = express();
+app.use(morgan('dev'));
 
 app.use(function (req, res, next) {
   console.log(req.headers);

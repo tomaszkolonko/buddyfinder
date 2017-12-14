25c223b87b204e52a780564282d7b7ac432eb58f
tomasz kolonko <tz.kolonko@gmail.com>
2017-12-14 10:46:34 +0100

    Added validation to registration

--- routes/pages.js
+++ routes/pages.js
@@ -3,6 +3,7 @@
 const Pages = require('../handlers/pages');
 const Assets = require('../handlers/assets');
 const Actions = require('../handlers/actions');
+const Joi = require('joi');
 
 module.exports = [{
     method: 'GET',
@@ -50,5 +51,30 @@ module.exports = [{
 }, {
     method: 'POST',
     path: '/register',
+    config: {
+        validate: {
+            payload: {
+                username: Joi.string().required(),
+                email: Joi.string().email().required(),
+                password: Joi.string().min(6).max(8)
+            },
+            options: {
+                abortEarly: false
+            },
+            failAction: function (request, reply, source, error) {
+                const errors = {};
+                const details = error.data.details;
+                for(let i = 0; i < details.length; ++i) {
+                    if (!errors.hasOwnProperty(details[i].path)) {
+                        errors[details[i].path] = details[i].message;
+                    }
+                }
+                reply.view('register', {
+                    errors: errors,
+                    values: request.payload
+                }).code(400);
+            }
+        }
+    },
     handler: Actions.register
 }];
